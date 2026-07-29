c89862be1ff37943c3c77cc65e4acc52ff0ed72d
Tarpi96 <tarpisingh.96@gmail.com>
2026-07-29 18:48:37 +0200

    feat: Update Recipe type to include optional fields for dietary preferences and additional metadata

--- lib/types.ts
+++ lib/types.ts
@@ -3,20 +3,43 @@ export type Recipe = {
   title: string;
   image: string;
   imageType: string;
+
   readyInMinutes: number;
   servings: number;
+
   vegetarian: boolean;
   vegan: boolean;
   glutenFree: boolean;
   dairyFree: boolean;
   veryHealthy: boolean;
-  cheap: boolean;
-  veryPopular: boolean;
+
+  cheap?: boolean;
+  veryPopular?: boolean;
+  sustainable?: boolean;
+  lowFodmap?: boolean;
+
   healthScore: number;
   pricePerServing: number;
+
+  weightWatcherSmartPoints?: number;
+  gaps?: string;
+
+  preparationMinutes?: number;
+  cookingMinutes?: number;
+
+  aggregateLikes?: number;
+
+  creditsText?: string;
+  license?: string;
+  sourceName?: string;
+  sourceUrl?: string;
+  summary?: string;
+
   cuisines: string[];
   dishTypes: string[];
   diets: string[];
+  occasions?: string[];
+
   nutrition: Nutrition;
 };
 
