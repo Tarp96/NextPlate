25ff1608c519e52ee0c70902fc6104df9ca52265
Tarpi96 <tarpisingh.96@gmail.com>
2026-07-29 13:13:02 +0200

    feat: Implement error handling for recipe fetching functions

--- lib/mock-spoonacular.ts
+++ lib/mock-spoonacular.ts
@@ -1,6 +1,7 @@
 import { mockRecipes } from "./mock-data";
 
 const MOCK_DELAY = 500;
+const SHOULD_FAIL = false;
 
 function delay(ms = MOCK_DELAY): Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, ms));
@@ -8,17 +9,27 @@ function delay(ms = MOCK_DELAY): Promise<void> {
 
 export async function getAllRecipes() {
   await delay();
+
+  if (SHOULD_FAIL) {
+    throw new Error("Failed to fetch Recipes");
+  }
   return mockRecipes;
 }
 
 export async function getRecipeById(id: number) {
   await delay();
-
+  if (SHOULD_FAIL) {
+    throw new Error("Failed to get recipe");
+  }
   return mockRecipes.find((recipe) => recipe.id === id);
 }
 
 export async function searchRecipeByName(query: string) {
   await delay();
+
+  if (SHOULD_FAIL) {
+    throw new Error(`Could not find recipe named ${query}`);
+  }
 
   return mockRecipes.filter((recipe) =>
     recipe.title.toLowerCase().includes(query.toLowerCase()),
