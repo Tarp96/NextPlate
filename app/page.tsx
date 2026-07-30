72104c4b784b353bf26a13d28191c5c4a10ab167
Tarpi96 <tarpisingh.96@gmail.com>
2026-07-30 09:12:10 +0200

    feat: Update Home component to fetch recipes asynchronously and display them in a list feat: Add RecipeCard and RecipeGrid components for future recipe display

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 import { getAllRecipes } from "@/lib/mock-spoonacular";
+import { Recipe } from "@/lib/types";
 
-export default function Home() {
-  const recipesList = getAllRecipes();
+export default async function Home() {
+  const recipesList: Recipe[] = await getAllRecipes();
+
+  const displayRecipes = recipesList.map((recipe) => (
+    <li key={recipe.id}>{recipe.title}</li>
+  ));
 
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <h1>NextPlate</h1>
+      <ul>{displayRecipes}</ul>
     </div>
   );
 }
