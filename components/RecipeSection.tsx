33a2b4b840262c5e49749afe607e462b32a3b776
Tarpi96 <tarpisingh.96@gmail.com>
2026-08-15 12:08:21 +0200

    feat: Refactor RecipeSection to implement filter functionality for Vegan, Vegetarian, Dairy Free, and Gluten Free options

--- components/RecipeSection.tsx
+++ components/RecipeSection.tsx
@@ -15,31 +15,49 @@ export default function RecipeSection({ recipeList }: RecipeSectionProps) {
   const [dairyFree, setIsDairyFree] = useState(false);
   const [glutenFree, setIsGlutenFree] = useState(false);
 
-  const displayRecipelist = isVegan
-    ? recipeList.filter((recipe) => recipe.vegan)
-    : recipeList;
+  const [filters, setFilters] = useState({
+    vegan: false,
+    vegetarian: false,
+    dairyFree: false,
+    glutenFree: false,
+  });
+
+  function toggleFilter(filter: keyof typeof filters) {
+    setFilters((prev) => ({
+      ...prev,
+      [filter]: !prev[filter],
+    }));
+  }
+
+  const displayRecipeList = recipeList.filter((recipe) => {
+    if (filters.vegan && !recipe.vegan) return false;
+    if (filters.vegetarian && !recipe.vegetarian) return false;
+    if (filters.dairyFree && !recipe.dairyFree) return false;
+    if (filters.glutenFree && !recipe.glutenFree) return false;
+
+    return true;
+  });
 
   return (
     <>
-      <FilterButton
-        label="Vegan"
-        onClickAction={() => setIsVegan((prev) => !prev)}
-      />
+      <FilterButton label="Vegan" onClickAction={() => toggleFilter("vegan")} />
 
       <FilterButton
         label="Vegetarian"
-        onClickAction={() => setIsVegetarian((prev) => !prev)}
-      />
-      <FilterButton
-        label="Dairy Free"
-        onClickAction={() => setIsDairyFree((prev) => !prev)}
-      />
-      <FilterButton
-        label="Gluten Free"
-        onClickAction={() => setIsGlutenFree((prev) => !prev)}
+        onClickAction={() => toggleFilter("vegetarian")}
       />
 
-      <RecipeGrid recipeList={displayRecipelist} />
+      <FilterButton
+        label="Dairy Free"
+        onClickAction={() => toggleFilter("dairyFree")}
+      />
+
+      <FilterButton
+        label="Gluten Free"
+        onClickAction={() => toggleFilter("glutenFree")}
+      />
+
+      <RecipeGrid recipeList={displayRecipeList} />
     </>
   );
 }
