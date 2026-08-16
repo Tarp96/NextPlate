226a1d2d7bfc952e56da7242850946f9eb73ab4f
Tarpi96 <tarpisingh.96@gmail.com>
2026-08-16 11:53:22 +0200

    feat: Enhance RecipeDetailsPage and RecipeCard with dynamic routing and link integration

--- app/recipeDetails/[id]/page.tsx
+++ app/recipeDetails/[id]/page.tsx
@@ -1,4 +1,6 @@
-export default function RecipeDetailsPage() {
-  return;
-  <h1></h1>;
+export default async function RecipeDetailsPage({ params }) {
+  const { id } = await params;
+
+  console.log(id);
+  return <h1>{`Your id ${id}`}</h1>;
 }
--- components/RecipeCard.tsx
+++ components/RecipeCard.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 type RecipeCardProps = {
   id: number;
@@ -10,6 +11,7 @@ type RecipeCardProps = {
 };
 
 export default function RecipeCard({
+  id,
   title,
   imageSrc,
   readyInMinutes,
@@ -17,30 +19,32 @@ export default function RecipeCard({
   vegan,
 }: RecipeCardProps) {
   return (
-    <article className="overflow-hidden rounded-xl bg-white shadow-md transition hover:-translate-y-1 hover:shadow-xl">
-      <div className="relative aspect-[4/3]">
-        <Image src={imageSrc} alt={title} fill className="object-cover" />
-      </div>
-
-      <div className="space-y-3 p-4">
-        <h2 className="line-clamp-2 text-lg font-semibold">{title}</h2>
-
-        <p className="text-sm text-gray-500">⏱ {readyInMinutes} min</p>
-
-        <div className="flex gap-2">
-          {vegetarian && (
-            <span className="rounded-full bg-green-100 px-3 py-1 text-xs font-medium text-green-700">
-              Vegetarian
-            </span>
-          )}
-
-          {vegan && (
-            <span className="rounded-full bg-emerald-100 px-3 py-1 text-xs font-medium text-emerald-700">
-              Vegan
-            </span>
-          )}
+    <Link href={`/recipeDetails/${id}`}>
+      <article className="overflow-hidden rounded-xl bg-white shadow-md transition hover:-translate-y-1 hover:shadow-xl">
+        <div className="relative aspect-[4/3]">
+          <Image src={imageSrc} alt={title} fill className="object-cover" />
         </div>
-      </div>
-    </article>
+
+        <div className="space-y-3 p-4">
+          <h2 className="line-clamp-2 text-lg font-semibold">{title}</h2>
+
+          <p className="text-sm text-gray-500">⏱ {readyInMinutes} min</p>
+
+          <div className="flex gap-2">
+            {vegetarian && (
+              <span className="rounded-full bg-green-100 px-3 py-1 text-xs font-medium text-green-700">
+                Vegetarian
+              </span>
+            )}
+
+            {vegan && (
+              <span className="rounded-full bg-emerald-100 px-3 py-1 text-xs font-medium text-emerald-700">
+                Vegan
+              </span>
+            )}
+          </div>
+        </div>
+      </article>
+    </Link>
   );
 }
