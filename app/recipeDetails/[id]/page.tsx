39cc8d69821bc90a84e90934dc6613d42f9e9ab9
Tarpi96 <tarpisingh.96@gmail.com>
2026-08-16 12:15:48 +0200

    feat: Refactor RecipeDetailsPage to include type definitions and improve code structure

--- app/recipeDetails/[id]/page.tsx
+++ app/recipeDetails/[id]/page.tsx
@@ -1,6 +1,19 @@
-export default async function RecipeDetailsPage({ params }) {
+import { getRecipeById } from "@/lib/mock-spoonacular";
+
+type RecipeDetailsPageProps = {
+  params: Promise<{
+    id: string;
+  }>;
+};
+
+export default async function RecipeDetailsPage({
+  params,
+}: RecipeDetailsPageProps) {
   const { id } = await params;
+  const recipeDetails = await getRecipeById(+id);
+
+  console.log(recipeDetails);
 
   console.log(id);
-  return <h1>{`Your id ${id}`}</h1>;
+  return <h1>{id}</h1>;
 }
