6636f540a5fd6c8abb315eb8f60ffa1a7514ddd7
tarpiSingh <tarpisingh.96@gmail.com>
2026-08-31 12:09:34 +0200

    feat: Add authentication check to profile page to avoid unlogged users accessing profile

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,7 +1,19 @@
-export default function ProfilePage() {
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+import { redirect } from "next/navigation";
+
+export default async function ProfilePage() {
+  const session = await getServerSession(authOptions);
+
+  if (!session?.user) {
+    redirect("/login");
+  }
+
+  const userName = session.user.name ?? "there";
+
   return (
     <>
-      <h1>Welcome to your profile!</h1>
+      <h1>Welcome {userName}</h1>
     </>
   );
 }
