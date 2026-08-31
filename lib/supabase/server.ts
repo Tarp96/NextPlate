7d3443a105b08115d5f8109ddeaba99614a433be
tarpiSingh <tarpisingh.96@gmail.com>
2026-08-31 13:02:44 +0200

    feat: Create supabase client

---
+++ lib/supabase/server.ts
@@ -0,0 +1,14 @@
+import { createClient } from "@supabase/supabase-js";
+
+export function createSupabase() {
+  const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const secretKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+
+  if (!url || !secretKey) {
+    throw new Error(
+      "Missing NEXT_PUBLIC_SUPABASE_URL or SUPABASE_SERVICE_ROLE_KEY",
+    );
+  }
+
+  return createClient(url, secretKey);
+}
