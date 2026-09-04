e84626d4faa71f3efb4d685ffba804733980b63e
tarpiSingh <tarpisingh.96@gmail.com>
2026-09-04 15:15:02 +0200

    feat: Create function for adding favorites

--- lib/actions/favorites.ts
+++ lib/actions/favorites.ts
@@ -8,4 +8,23 @@ export async function addFavorites(recipe: {
   image: string;
 }) {
   const session = await getAuthSession();
+
+  if (!session?.user.id) {
+    return { error: "You must be logged in" };
+  }
+
+  const supabase = createSupabase();
+
+  const { error } = await supabase.from("favorites").insert({
+    user_id: session.user.id,
+    recipe_id: recipe.id,
+    title: recipe.title,
+    image: recipe.image,
+  });
+
+  if (error) {
+    return { error: error.message };
+  }
+
+  return { success: true };
 }
