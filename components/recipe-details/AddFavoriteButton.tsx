36e221fd3d716d418a5d737dba0aef8c64bfc3bd
tarpiSingh <tarpisingh.96@gmail.com>
2026-09-04 15:37:51 +0200

    feat: Implement AddFavoriteButton component for adding recipes to favorites

--- components/recipe-details/AddFavoriteButton.tsx
+++ components/recipe-details/AddFavoriteButton.tsx
@@ -1,7 +1,22 @@
 "use client";
+import { addFavorites } from "@/lib/actions/favorites";
 
 export default function AddFavoriteButton({
   recipe,
 }: {
   recipe: { id: number; title: string; image: string };
-}) {}
+}) {
+  return (
+    <button
+      onClick={() =>
+        addFavorites({
+          id: recipe.id,
+          title: recipe.title,
+          image: recipe.image,
+        })
+      }
+    >
+      Add to Favorites
+    </button>
+  );
+}
